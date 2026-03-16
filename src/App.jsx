d920ec248af85202230f46a7ce659554ac002d15
Lucas alvez <luc4salvez0@gmail.com>
2026-03-16 01:34:42 -0300

    feat: add Sidebar and sidebarButton component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import Sidebar from "./components/Sidebar"
+
 function App() {
   return (
     <>
-      <h1 className="">Hello world</h1>
+      <Sidebar />
     </>
   )
 }
---
+++ src/components/SidebarButton.jsx
@@ -0,0 +1,26 @@
+import PropTypes from "prop-types"
+
+const SidebarButton = ({ children, variant }) => {
+  const getVariantClasses = () => {
+    if (variant === "unselected") {
+      return "text-[#35383E]"
+    }
+    if (variant === "selected") {
+      return "bg-[#E6F7F8] text-[#00ADB5]"
+    }
+  }
+
+  return (
+    <a href="#" className={`rounded-lg px-6 py-3 ${getVariantClasses()}`}>
+      {children}
+    </a>
+  )
+}
+
+// Adicione as PropTypes para o ESLint parar de reclamar
+SidebarButton.propTypes = {
+  children: PropTypes.node.isRequired,
+  variant: PropTypes.oneOf(["unselected", "selected"]).isRequired,
+}
+
+export default SidebarButton
