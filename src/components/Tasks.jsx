75ffe9886296819acd1f6630820b9c1c69275635
Lucas alvez <luc4salvez0@gmail.com>
2026-03-18 16:48:15 -0300

    feat: add task separator

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,6 +1,9 @@
 import Button from "./Button"
 import AddIcon from "../assets/icons/add.svg?react"
 import TrashIcon from "../assets/icons/trash.svg?react"
+import SunIcon from "../assets/icons/sun.svg?react"
+import CloudIcon from "../assets/icons/cloud-sun.svg?react"
+import MoonIcon from "../assets/icons/moon.svg?react"
 
 function Tasks() {
   return (
@@ -21,6 +24,28 @@ function Tasks() {
             Nova tarefa
             <AddIcon />
           </Button>
+        </div>
+      </div>
+      <div className="rounded-xl bg-white p-6">
+        <div className="space-y-3">
+          <div className="flex gap-2 border-b border-solid border-[#f4f4f5] pb-1">
+            <SunIcon />
+            <p className="text-sm text-[#9A9C9F]">Manhã</p>
+          </div>
+        </div>
+
+        <div className="my-24 space-y-3">
+          <div className="flex gap-2 border-b border-solid border-[#f4f4f5] pb-1">
+            <CloudIcon />
+            <p className="text-sm text-[#9A9C9F]">Tarde</p>
+          </div>
+        </div>
+
+        <div className="space-y-3">
+          <div className="flex gap-2 border-b border-solid border-[#f4f4f5] pb-1">
+            <MoonIcon />
+            <p className="text-sm text-[#9A9C9F]">Noite</p>
+          </div>
         </div>
       </div>
     </div>
